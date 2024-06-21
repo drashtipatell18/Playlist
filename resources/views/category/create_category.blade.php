ed8b16b176651f447ee5dba38035aec054680947
drashtipatell18 <drashti.kalathiyainfotech@gmail.com>
2024-06-21 11:01:53 +0530

    insert category code

--- resources/views/category/create_category.blade.php
+++ resources/views/category/create_category.blade.php
@@ -1,25 +1,26 @@
 @extends('layouts.main')
 @section('content')
-<div class="card">
-    <div class="card-body">
-      <h5 class="card-title">Create Category</h5>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Create Category</h5>
 
-      <!-- General Form Elements -->
-      <form>
-        <div class="row mb-3">
-          <label for="inputText" class="col-sm-2 col-form-label">Category Name</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" name="category_name">
-          </div>
+            <!-- General Form Elements -->
+            <form action="{{ isset($category) ? '/category/update/' . $category->id : '/category/store' }}" method="POST">
+                @csrf
+                <div class="row mb-3">
+                    <label for="inputText" class="col-sm-2 col-form-label">Category Name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="category_name">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-sm-10 text-center">
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </div>
+
+            </form><!-- End General Form Elements -->
+
         </div>
-        <div class="row mb-3">
-          <div class="col-sm-10 text-center">
-            <button type="submit" class="btn btn-primary">Save</button>
-          </div>
-        </div>
-
-      </form><!-- End General Form Elements -->
-
     </div>
-  </div>
 @endsection
--- resources/views/category/view_category.blade.php
+++ resources/views/category/view_category.blade.php
@@ -6,26 +6,41 @@
             <h5 class="card-title">Category List</h5>
             <div>
                 <a href="{{ route('category.create') }}">
-                    <button type="button" class="btn btn-primary btn-sm mt-1" id="addCategoryBtn"><i class="bi bi-plus-lg"></i> Add Category</button>
+                    <button type="button" class="btn btn-primary btn-sm mt-1" id="addCategoryBtn"><i class="bi bi-plus-lg"></i>
+                        Add Category</button>
                 </a>
             </div>
         </div>
-        
+
         <div class="card-body pb-0">
+            <div class="table-responsive mt-3">
+                <table class="table table-striped jambo_table bulk_action" id="table">
+                    <thead>
+                        <tr>
+                            <th class="text-center">ID</th>
+                            <th class="text-center">Name</th>
+                            <th class="text-center">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($categorys as $index => $category)
+                            <tr class="">
+                                <td class="text-center">{{ $index + 1 }}</td>
+                                <td class="text-center">{{ $category->category_name }}</td>
+                                <td class="text-center">
+                                    <a href="{{ route('edit.category', $category->id) }}" class="btn btn-info btn-sm"><i
+                                            class="bi bi-pencil-square"></i></a>
+                                    <a href="{{ route('destroy.category', $category->id) }}" class="btn btn-danger btn-sm"
+                                        onclick="return confirm('Are you sure you want to delete this ?');"><i
+                                            class="bi bi-trash3-fill"></i></a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
 
-            <table class="table table-borderless">
-                <thead>
-                    <tr>
-                        <th class="text-center">ID</th>
-                        <th class="text-center">Name</th>
-                        <th class="text-center">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                </tbody>
-            </table>
-
+            
         </div>
     </div>
 @endsection
